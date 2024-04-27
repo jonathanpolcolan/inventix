08d245f60ef84e3b6c876de0043cd9b7505d3db4
jonathanpolcolan <jonshaitan@gmail.com>
2024-04-27 17:06:36 -0500

    DIA 2 CON 3 HTML

--- botones.js
+++ botones.js
@@ -6,9 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     button.addEventListener('click', function() {
         // Cambia la ubicación para redirigir al usuario
         window.location.href = 'login.html'; // Modifica esta URL por la página a la que desees redirigir
-   
     });
-
 
      // Selecciona el botón por su ID
      var button = document.getElementById('meseroButton');
