3926c4941c86f4c7e8cdbc9c7d583801af3ba854
jonathanpolcolan <jonshaitan@gmail.com>
2024-04-27 13:59:51 -0500

    DIA 2

---
+++ botones.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona el botón por su ID
+    var button = document.getElementById('adminButton');
+
+    // Agrega un manejador de eventos para el clic
+    button.addEventListener('click', function() {
+        // Cambia la ubicación para redirigir al usuario
+        window.location.href = 'login.html'; // Modifica esta URL por la página a la que desees redirigir
+   
+    });
+
+
+     // Selecciona el botón por su ID
+     var button = document.getElementById('meseroButton');
+
+     // Agrega un manejador de eventos para el clic
+     button.addEventListener('click', function() {
+         // Cambia la ubicación para redirigir al usuario
+         window.location.href = 'login.html'; // Modifica esta URL por la página a la que desees redirigir
+        });
+});
